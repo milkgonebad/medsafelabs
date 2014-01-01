a8ad97713f86fb25d7d8fe3d625cbf290fd4085f
Amy Black <amy@gothicmaine.com>
2014-01-01 13:36:35 -0500

    add staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,7 +83,7 @@ MedSafeLabs::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'], # MedSafeLabsLlc
+      :bucket => ENV['S3_BUCKET_NAME'], # MedSafeLabsLlcStaging
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     } 
@@ -93,7 +93,7 @@ end
 
 MedSafeLabs::Application.config.middleware.use ExceptionNotification::Rack,
   :email => {
-    :email_prefix => "[ERROR MedSafeLabs] ",
+    :email_prefix => "[ERROR MedSafeLabs STAGING] ",
     :sender_address => %{"notifier" <notifier@medsafelabs.com>},
     :exception_recipients => %w{gothicmaine@gmail.com}
   }
